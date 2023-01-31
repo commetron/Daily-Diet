6b368ba99771d20139a591993aca797cd5be072b
commetron <softechstar.pdf@outlook.com>
2023-01-31 18:04:52 -0300

    feat:  add empty component in section list

---
+++ src/components/empty/index.tsx
@@ -0,0 +1,9 @@
+import * as Styled from './styles';
+
+export const Empty = () => {
+  return (
+    <Styled.Container>
+      <Styled.Title>List vazia, que tal cadastrar algo ?</Styled.Title>
+    </Styled.Container>
+  );
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Empty } from '@components/empty';
 import { Header } from '@components/Header';
 import { MealCard } from '@components/MealCard';
 import { PercentCard } from '@components/PercentCard';
@@ -6,11 +7,10 @@ import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 import { SectionList } from 'react-native';
 import { DietListProps, DataProps } from './HomeProps';
-import { mock } from './mock';
 import * as Styled from './styles';
 
 export const Home = () => {
-  const [dietList, setDietList] = useState<DietListProps[]>(mock);
+  const [dietList, setDietList] = useState<DietListProps[]>([]);
   const [title, setTitle] = useState('50,50%');
   const [type, setType] = useState(true);
   const { navigate } = useNavigation();
@@ -47,6 +47,8 @@ export const Home = () => {
         renderSectionHeader={({ section: { title } }) => <Styled.SectionTitle>{title}</Styled.SectionTitle>}
         showsVerticalScrollIndicator={false}
         style={{ flex: 1, marginVertical: 16 }}
+        contentContainerStyle={dietList.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <Empty />}
       />
     </Styled.Container>
   );
